cd7de35cc9d9591b854fe15b8f66b15ae3c49064
money3222006-png <money3222006@gmail.com>
2026-02-23 18:35:07 +0800

    Update recruit.js

--- recruit.js
+++ recruit.js
@@ -13,7 +13,7 @@ const workersData = [
         experience: 'expert',
         employmentType: ['full-time', 'freelance'],
         skills: ['React', 'Vue.js', 'JavaScript', 'CSS', 'TypeScript'],
-        image: 'https://via.placeholder.com/150?text=Sarah'
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fiphone%2Fcomments%2F1azcyjo%2Fi_keep_finding_random_photos_saved_on_my_iphone%2F&psig=AOvVaw1w4sL6yrQrljZVCsh72-UL&ust=1771929270555000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIjz-4i175IDFQAAAAAdAAAAABAE'
     },
     {
         id: 2,
@@ -354,4 +354,5 @@ function updateResultCount() {
     const count = filteredWorkers.length;
     document.getElementById('resultCount').textContent = 
         count === 0 ? 'No workers found' : `Showing ${count} worker${count !== 1 ? 's' : ''}`;
+
 }
